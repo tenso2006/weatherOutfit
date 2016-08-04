573982bd9b6327116ee3c299a376dd8acc12ec4f
Tenzin Sonam <tenso2006@gmail.com>
2016-08-04 15:52:03 -0700

    first working version

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("Test")
+var sunnyButton = document.getElementById("sunny")
+var rainyButton = document.getElementById("rainy")
+var recommenderDiv = document.getElementById("recommender")
+sunnyButton.onclick = function (){
+	console.log("Today is sunny");
+}
+rainyButton.onclick = function (){
+	console.log("Today is rainy");
+}
